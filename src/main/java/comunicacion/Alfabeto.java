42621fe2242d0b4f650f785c85c7d4de089559ab
alejandrons <80337888+alejandrons@users.noreply.github.com>
2021-12-07 23:29:03 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -4,7 +4,7 @@ public class Alfabeto extends Pictograma {
     private String[] letras;
     private String interpretacion;
 
-    public Alfabeto(String interpretacion, String origen, String[] letras) {
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
         super(origen);
         this.letras = letras;
         this.interpretacion = interpretacion;
