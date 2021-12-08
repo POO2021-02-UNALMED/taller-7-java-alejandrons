190b364744e82ffba14a3e1dcacbe046705ec295
alejandrons <80337888+alejandrons@users.noreply.github.com>
2021-12-07 23:10:06 -0500

    Solucion

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,41 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+    private String[] letras;
+    private String interpretacion;
+
+    public Alfabeto(String[] letras, String interpretacion, String origen) {
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String interpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    } 
+    
+    public int cantidadLetras(){
+        return letras.length;
+    }
+    
+    @Override
+    public String toString(){
+        String pantalla = letras[0];
+        for (int i=1; i<letras.length; i++){
+            pantalla = pantalla + ", " + letras[i];
+        }
+        return pantalla;
+    }
+}
